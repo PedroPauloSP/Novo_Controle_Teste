697984a136c01d487061aee134c1e571e631360f
PedroPaulo <pedropsantospereira@gmail.com>
2026-05-17 10:15:13 -0300

    Iplementados testes da aplicação

--- src/test/java/com/Api/MaterialEstocado/MaterialEstocadoApplicationTests.java
+++ src/test/java/com/Api/MaterialEstocado/MaterialEstocadoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.Api.MaterialEstocado;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +10,11 @@ class MaterialEstocadoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+  @Test
+    void main() {
 
+        assertDoesNotThrow(() -> {
+            MaterialEstocadoApplication.main(new String[]{});
+        });
+}
 }
